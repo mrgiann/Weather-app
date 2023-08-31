2b4afad1d2c6b2d228838e83b4626e0a7986614d
Renzo Roggiapane <82038942+mrgiann@users.noreply.github.com>
2023-08-31 16:15:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,6 +19,26 @@ searchInput.addEventListener("keydown", event => {
   }
 });
 
+function getWindDirectionDescription(degrees) {
+  if (degrees >= 337.5 || degrees < 22.5) {
+    return "Norte";
+  } else if (degrees >= 22.5 && degrees < 67.5) {
+    return "Noreste";
+  } else if (degrees >= 67.5 && degrees < 112.5) {
+    return "Este";
+  } else if (degrees >= 112.5 && degrees < 157.5) {
+    return "Sureste";
+  } else if (degrees >= 157.5 && degrees < 202.5) {
+    return "Sur";
+  } else if (degrees >= 202.5 && degrees < 247.5) {
+    return "Suroeste";
+  } else if (degrees >= 247.5 && degrees < 292.5) {
+    return "Oeste";
+  } else if (degrees >= 292.5 && degrees < 337.5) {
+    return "Noroeste";
+  }
+}
+
 function searchWeather() {
   const searchValue = searchInput.value.trim();
 
@@ -31,35 +51,36 @@ function searchWeather() {
         const temperature = data.main.temp;
         const weatherDescription = data.weather[0].description;
         const cityName = data.name;
+        const country = data.sys.country;
         const feelsLike = data.main.feels_like;
         const humidity = data.main.humidity;
         const pressure = data.main.pressure;
-        const windSpeed = data.wind.speed;
+        const windSpeed = data.wind.speed * 3.6;
         const windDirection = data.wind.deg;
-        const weatherIconCode = data.weather[0].icon; // Nuevo código para el icono
+        const windDirectionDescription = getWindDirectionDescription(windDirection);
+        const weatherIconCode = data.weather[0].icon;
 
         errorElement.innerHTML = "";
-        cityElement.innerHTML = `Resultados para ${data.name}`;
+        cityElement.innerHTML = `Resultados para ${cityName}, ${country}`;
         temperatureDisplay.innerHTML = `<p>${temperature}°C</p>`;
 
         additionalInfo.innerHTML = `
         <li>Sensación térmica: ${feelsLike}°C</li>
         <li>Humedad: ${humidity}%</li>
         <li>Presión: ${pressure} hPa</li>
-        <li>Velocidad del viento: ${windSpeed} m/s</li>
-        <li>Dirección del viento: ${windDirection}°</li>
+        <li>Velocidad del viento: ${windSpeed.toFixed(2)} km/h</li>
+        <li>Dirección del viento: ${windDirectionDescription} (${windDirection}°)</li>
         <li>Nubes: ${weatherDescription}</li>
         `;
 
-        // Insertar el icono del clima
         weatherIcon.src = `http://openweathermap.org/img/w/${weatherIconCode}.png`;
       })
       .catch(error => {
         errorElement.innerHTML = "<p>No se pudo obtener la información del clima para esa ubicación.</p>";
-        additionalInfo.innerHTML = ""; // Limpiar la información adicional
-        cityElement.innerHTML = ""; // Limpia la ciudad
-        temperatureDisplay.innerHTML = "" // Limpia la temperatura
-        weatherIcon.src = ""; // Limpia el icono
+        additionalInfo.innerHTML = "";
+        cityElement.innerHTML = "";
+        temperatureDisplay.innerHTML = ""
+        weatherIcon.src = "";
 
         console.error(error);
       });
